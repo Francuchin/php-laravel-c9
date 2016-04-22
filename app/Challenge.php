782fa2330455d0e5b4a6672f7f7c2cae930870b5
Francuchin <francuchin@gmail.com>
2016-04-22 00:13:46 -0300

    prueba

--- app/Challenge.php
+++ app/Challenge.php
@@ -9,6 +9,7 @@ class Challenge extends Model
     protected $table = 'challenges';
     protected $fillable = ['title','description', 'id_user'];
     protected $guarded = ['id'];
+    
     public function user(){
         return $this->belongsTo('App\User','id_user');
     }
